3a818a1f4c93636c321d58515e875087fb09f540
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-03-10 11:31:31 +0700

    first commit

---
+++ Worker.cs
@@ -0,0 +1,22 @@
+namespace WeatherBackend
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //Console.WriteLine("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
